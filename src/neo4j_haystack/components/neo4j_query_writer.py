0d93b2102c6b677739cb878316c711ddd4a890d2
Sergey Bondarenco <sergey.bondarenco@outlook.com>
2024-09-17 15:24:57 +0300

    refactor: Adjust component outputs and parameter serialization for Neo4jQueryWriter

--- src/neo4j_haystack/components/neo4j_query_writer.py
+++ src/neo4j_haystack/components/neo4j_query_writer.py
@@ -1,22 +1,22 @@
 import logging
-from typing import Any, Dict, List, Literal, Optional, TypedDict
+from typing import Any, Dict, List, Literal, Optional
 
 from haystack import component, default_from_dict, default_to_dict
-from neo4j import ResultSummary
+from typing_extensions import NotRequired, TypedDict
 
 from neo4j_haystack.client import Neo4jClient, Neo4jClientConfig
-from neo4j_haystack.components.utils import (
-    DataclassParameterMarshaller,
-    DocumentParameterMarshaller,
-    Neo4jQueryParameterMarshaller,
+from neo4j_haystack.serialization import (
+    Neo4jQueryParametersMarshaller,
+    QueryParametersMarshaller,
 )
 
 logger = logging.getLogger(__name__)
 
 
 class QueryResult(TypedDict):
-    query_type: Optional[str]
     query_status: Literal["success", "error"]
+    error_message: NotRequired[str]
+    error: NotRequired[Exception]
 
 
 @component
@@ -24,27 +24,16 @@ class Neo4jQueryWriter:
     """
     A component for writing arbitrary data to Neo4j database using plain Cypher query.
 
-    This component gives flexible way to write data to Neo4j by running arbitrary Cypher query along with query
+    This component gives flexible way to write data to Neo4j by running arbitrary Cypher query with
     parameters. Query parameters can be supplied in a pipeline from other components (or pipeline data).
     You could use such queries to write Documents with additional graph nodes for a more complex RAG scenarios.
-
-    See the following documentation on how to compose Cypher queries with parameters:
-
-    - [Overview of Cypher query syntax](https://neo4j.com/docs/cypher-manual/current/queries/)
-    - [Cypher Query Parameters](https://neo4j.com/docs/cypher-manual/current/syntax/parameters/)
-
-    Above are resources which will help understand better Cypher query syntax and parameterization. Under the hood
-    [Neo4j Python Driver](https://neo4j.com/docs/python-manual/current/) is used to write data to Neo4j.
-    Check the following documentation:
-
-    - [Query the database](https://neo4j.com/docs/python-manual/current/query-simple/)
-    - [Query parameters](https://neo4j.com/docs/python-manual/current/query-simple/#query-parameters)
-    - [Data types and mapping to Cypher types](https://neo4j.com/docs/python-manual/current/data-types/)
+    The difference between [DocumentWriter](https://docs.haystack.deepset.ai/docs/documentwriter) and `Neo4jQueryWriter`
+    is that the latter can write any data to Neo4j - not just Documents.
 
     Note:
-        Please consider data types mappings in Cypher query when working with parameters. Neo4j Python Driver handles
-        type conversions/mappings. Specifically you can figure out in the documentation of the driver how to work with
-        temporal types (e.g. `DateTime`).
+        Please consider [data types mappings](https://neo4j.com/docs/api/python-driver/current/api.html#data-types) in \
+        Cypher query when working with query parameters. Neo4j Python Driver handles type conversions/mappings.
+        Specifically you can figure out in the documentation of the driver how to work with temporal types.
 
     ```py title="Example: Creating a Document node with Neo4jQueryWriter"
     from neo4j_haystack.client.neo4j_client import Neo4jClientConfig
@@ -67,52 +56,57 @@ class Neo4jQueryWriter:
     ```
 
     Output:
-        `>>> {'result_available_after': 4, 'result_consumed_after': 0, 'query_type': 'w'}`
+        `>>> {'query_status': 'success'}`
 
-    The above example shows the flexibility of the `Neo4jQueryWriter` component:
+    In case query execution results in error and `raise_on_failure=False` the output will contain the error, e.g.:
 
-    - Cypher query can practically write any data to Neo4j depending on your use case
-    - Parameters can be provided at the component creation time, see `parameters`
-    - In RAG pipeline runtime parameters could be connected from other components.
-      Make sure during creation time to specify which `runtime_parameters` are expected.
+    Output:
+        `>>> {'query_status': 'error', 'error_message': 'Invalid cypher syntax', error: <Exception>}`
+
+    In RAG pipeline runtime parameters could be connected from other components. Make sure during component creation to
+    specify which `runtime_parameters` are expected to become as input slots for the component. In the example above
+    `doc_meta` can be connected , e.g. `pipeline.connect("other_component.output", "writer.doc_meta")`.
 
     Important:
         At the moment parameters support simple data types, dictionaries (see `doc_meta` in the example above) and
         python dataclasses (which can be converted to `dict`). For example `haystack.Document` or `haystack.ChatMessage`
-        instances are valid query parameter inputs. However, currently Neo4j Python Driver does not support
-        dataclasses/types with nested attributes. `haystack.Document` when serialized to dictionary flattens its meta
-        attributes. `haystack.ChatMessage`, for example, has custom serializer `DataclassParameterMarshaller` which
-        automatically flattens nested attributes like `meta`. So when you pass a \
-        `ChatMessage.from("Hello", meta={"year": 2017})` instance to the query as a parameter its dictionary form will
-        be as follows: `{role: 'user', 'meta.year': 2017}`
+        instances are valid query parameter inputs. However, currently Neo4j Python Driver does not convert dataclasses
+        to dictionaries automatically for us. By default \
+            [Neo4jQueryParametersMarshaller][neo4j_haystack.serialization.query_parameters_marshaller.Neo4jQueryParametersMarshaller]
+        is used to handle such conversions. You can change this logic by creating your own marshaller (see the
+        `query_parameters_marshaller` attribute)
     """
 
     def __init__(
         self,
         client_config: Neo4jClientConfig,
+        query: Optional[str] = None,
         runtime_parameters: Optional[List[str]] = None,
         verify_connectivity: Optional[bool] = False,
         raise_on_failure: bool = True,
+        query_parameters_marshaller: Optional[QueryParametersMarshaller] = None,
     ):
         """
         Create a Neo4jDocumentWriter component.
 
         Args:
             client_config: Neo4j client configuration to connect to database (e.g. credentials and connection settings).
+            query: Optional Cypher query for document retrieval. If `None` should be provided as component input.
             runtime_parameters: list of input parameters/slots for connecting components in a pipeline.
             verify_connectivity: If `True` will verify connectivity with Neo4j database configured by `client_config`.
             raise_on_failure: If `True` raises an exception if it fails to execute given Cypher query.
+            query_parameters_marshaller: Marshaller responsible for converting query parameters which can be used in
+                Cypher query, e.g. python dataclasses to be converted to dictionary. `Neo4jQueryParametersMarshaller`
+                is the default marshaller implementation.
         """
         self._client_config = client_config
+        self._query = query
         self._runtime_parameters = runtime_parameters or []
         self._verify_connectivity = verify_connectivity
         self._raise_on_failure = raise_on_failure
 
         self._neo4j_client = Neo4jClient(client_config)
-        self._marshallers: List[Neo4jQueryParameterMarshaller] = [
-            DocumentParameterMarshaller(),
-            DataclassParameterMarshaller(),
-        ]
+        self._query_parameters_marshaller = query_parameters_marshaller or Neo4jQueryParametersMarshaller()
 
         # setup inputs
         run_input_slots = {"query": str, "parameters": Optional[Dict[str, Any]]}
@@ -120,7 +114,7 @@ class Neo4jQueryWriter:
         component.set_input_types(self, **run_input_slots, **kwargs_input_slots)
 
         # setup outputs
-        component.set_output_types(self, query_type=Optional[str], query_status=str)
+        component.set_output_types(self, query_status=str, error=Optional[Exception], error_message=Optional[str])
 
         if verify_connectivity:
             self._neo4j_client.verify_connectivity()
@@ -131,6 +125,7 @@ class Neo4jQueryWriter:
         """
         data = default_to_dict(
             self,
+            query=self._query,
             runtime_parameters=self._runtime_parameters,
             verify_connectivity=self._verify_connectivity,
             raise_on_failure=self._raise_on_failure,
@@ -149,7 +144,7 @@ class Neo4jQueryWriter:
         data["init_parameters"]["client_config"] = client_config
         return default_from_dict(cls, data)
 
-    def run(self, query: str, parameters: Optional[Dict[str, Any]] = None, **kwargs) -> QueryResult:
+    def run(self, query: Optional[str] = None, parameters: Optional[Dict[str, Any]] = None, **kwargs) -> QueryResult:
         """
         Runs the arbitrary Cypher `query` with `parameters` to write data to Neo4j.
 
@@ -158,70 +153,44 @@ class Neo4jQueryWriter:
         Args:
             query: Cypher query to run.
             parameters: Cypher query parameters which can be used as placeholders in the `query`.
-            kwargs: Arbitrary parameters supplied in a pipeline execution from other component's output slots, e.g.
+            kwargs: Runtime parameters from connected components in a pipeline, e.g.
                 `pipeline.connect("year_provider.year_start", "writer.year_start")`, where `year_start` will be part
                 of `kwargs`.
 
         Returns:
             Output: Query execution stats.
 
-                Example: `:::py {'result_available_after': 4, 'result_consumed_after': 0, 'query_type': 'w'}`
-
-                where:
-
-                - `result_available_after` - The time it took for the server to have the result available. (in 'ms')
-                - `result_consumed_after` - The time it took for the server to consume the result. (in 'ms')
-                - `query_type` - A string that describes the type of query (e.g. 'r', 'rw', 'w', 's')
+                Example: `:::py {'query_status': 'success'}`
         """
+        query = query or self._query
+        if query is None:
+            raise ValueError(
+                "`query` is mandatory input and should be provided either in component's constructor, pipeline input or"
+                "connection"
+            )
         kwargs = kwargs or {}
         parameters = parameters or {}
         parameters_combined = {**kwargs, **parameters}
 
         try:
-            result_summary, _ = self._neo4j_client.execute_write(
-                query, parameters=self._serialize_parameters(parameters_combined)
+            self._neo4j_client.execute_write(
+                query,
+                parameters=self._serialize_parameters(parameters_combined),
             )
 
-            return self._query_result(result_summary)
+            return {"query_status": "success"}
         except Exception as ex:
             if self._raise_on_failure:
+                logger.error("Couldn't execute Neo4j write query %s", ex)
                 raise ex
-            logger.error("Couldn't execute Neo4j write query %s", ex)
-            return self._query_result()
+
+            return {"query_status": "error", "error_message": str(ex), "error": ex}
 
     def _serialize_parameters(self, parameters: Any) -> Any:
         """
-        Serializes `parameters` into data structure which can be accepted by Neo4j Python Driver (and a Cypher query
-        respectively). See the following marshallers with implementation details:
-
-        - [DocumentParameterMarshaller][neo4j_haystack.components.utils.DocumentParameterMarshaller] - converts
-            `haystack.Document` instances to a dictionary
-        - [DataclassParameterMarshaller][neo4j_haystack.components.utils.DataclassParameterMarshaller] - converts
-            dataclasses to a dictionary with flattened nested attributes
-
-        Args:
-            parameters: Any object which is passed as a Cypher query parameter
-
-        Returns:
-            Serialized parameters
+        Serializes `parameters` into a data structure which can be accepted by Neo4j Python Driver (and a Cypher query
+        respectively). See \
+            [Neo4jQueryParametersMarshaller][neo4j_haystack.serialization.query_parameters_marshaller.Neo4jQueryParametersMarshaller]
+            for more details.
         """
-        if isinstance(parameters, dict):
-            return {key: self._serialize_parameters(value) for key, value in parameters.items()}
-        elif isinstance(parameters, (list, tuple, set)):
-            return [self._serialize_parameters(value) for value in parameters]
-
-        # Find a marshaller which supports given type of parameter otherwise return parameter as is
-        marshaller = next((m for m in self._marshallers if m.supports(parameters)), None)
-        return marshaller.marshal(parameters) if marshaller else parameters
-
-    def _query_result(self, result_summary: Optional[ResultSummary] = None) -> QueryResult:
-        if result_summary:
-            return {
-                "query_type": result_summary.query_type,
-                "query_status": "success",
-            }
-        else:
-            return {
-                "query_type": "w",
-                "query_status": "error",
-            }
+        return self._query_parameters_marshaller.marshal(parameters)
--- tests/neo4j_haystack/components/test_neo4j_query_writer.py
+++ tests/neo4j_haystack/components/test_neo4j_query_writer.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+from typing import Any, Dict
 from unittest import mock
 
 import pytest
@@ -35,7 +37,7 @@ def test_query_writer(client_config: Neo4jClientConfig, neo4j_client: Neo4jClien
         doc_meta=doc_meta,
     )
 
-    assert result["query_type"] == "w"
+    assert result["query_status"] == "success"
 
     records = list(
         neo4j_client.query_nodes("MATCH (doc:Document {id:$doc_id}) RETURN doc", parameters={"doc_id": doc_id})
@@ -67,7 +69,7 @@ def test_query_writer_with_document_input(client_config: Neo4jClientConfig, neo4
         **runtime_params,  # Document will be serialized with `Document.to_dict`
     )
 
-    assert result["query_type"] == "w"
+    assert result["query_status"] == "success"
 
     records = list(
         neo4j_client.query_nodes("MATCH (doc:Document {id:$doc_id}) RETURN doc", parameters={"doc_id": doc_id})
@@ -76,6 +78,63 @@ def test_query_writer_with_document_input(client_config: Neo4jClientConfig, neo4
 
     doc_in_neo4j = records[0].data().get("doc")
     assert Document.from_dict(doc_in_neo4j) == document
+
+
+@pytest.mark.integration
+def test_query_writer_with_complex_input(client_config: Neo4jClientConfig, neo4j_client: Neo4jClient):
+    @dataclass
+    class Coordinates:
+        x: float
+        y: float
+
+    @dataclass
+    class Location:
+        area: str
+        coordinates: Coordinates
+
+    query = """
+            MERGE (p:`Person` {name: $name})
+            SET p += {
+                    age: $age,
+                    milestones: $milestones,
+                    `first_friend.name`: $friends[0].name,
+                    `first_friend.age`: $friends[0].age,
+                    `location.area`: $location.area,
+                    `location.coordinates.x`: $location.coordinates.x,
+                    `location.coordinates.y`: $location.coordinates.y
+                }
+        """
+
+    writer = Neo4jQueryWriter(client_config=client_config, query=query)
+
+    runtime_params = {
+        "name": "emily",
+        "age": 30,
+        "milestones": ["first", "second"],
+        "friends": [{"name": "john", "age": 29}],
+        "interests": {"summer": "swimming", "winter": "skiing"},
+        "location": Location("Nice", Coordinates(51.4934, 0.0098)),
+    }
+    result = writer.run(parameters=runtime_params)
+
+    assert result["query_status"] == "success"
+
+    records = list(
+        neo4j_client.query_nodes("MATCH (person:Person {name:$name}) RETURN person", parameters={"name": "emily"})
+    )
+    assert len(records) == 1
+
+    person_in_neo4j: Dict[str, Any] = records[0].data().get("person", {})
+    assert person_in_neo4j == {
+        "name": "emily",
+        "age": 30,
+        "milestones": ["first", "second"],
+        "first_friend.name": "john",
+        "first_friend.age": 29,
+        "location.area": "Nice",
+        "location.coordinates.x": 51.4934,
+        "location.coordinates.y": 0.0098,
+    }
 
 
 @pytest.mark.integration
@@ -97,14 +156,14 @@ def test_query_writer_with_dataclass_input(client_config: Neo4jClientConfig, neo
     WITH session
     CREATE (session)-[:FIRST_MESSAGE]->(msg_node:`ChatMessage`)
     WITH msg_node
-    MATCH (msg_node) SET msg_node += $chat_messages[0]
+    MATCH (msg_node) SET msg_node += { role: $chat_messages[0].role, content: $chat_messages[0].content }
     WITH msg_node as first_message
 
     // Create remaining chat message nodes
     UNWIND tail($chat_messages) as tail_message
     CREATE (msg_node:`ChatMessage`)
     WITH msg_node, tail_message, first_message
-    MATCH (msg_node) SET msg_node += tail_message
+    MATCH (msg_node) SET msg_node += { role: tail_message.role, content: tail_message.content }
 
     // Connect chat messages with :NEXT_MESSAGE relationship
     WITH collect(msg_node) AS message_nodes, first_message
@@ -122,7 +181,7 @@ def test_query_writer_with_dataclass_input(client_config: Neo4jClientConfig, neo
         **runtime_params,  # Will be serialized to list of dictionaries
     )
 
-    assert result["query_type"] == "w"
+    assert result["query_status"] == "success"
 
     cypher_query_get_messages = """
     MATCH (session:Session {id:$session_id})-[:FIRST_MESSAGE|NEXT_MESSAGE*]->(message)
@@ -146,6 +205,25 @@ def test_query_writer_with_dataclass_input(client_config: Neo4jClientConfig, neo
         {"role": "assistant", "content": "How can I help you?"},
         {"role": "user", "content": "Tell me your secret"},
     ]
+
+
+@pytest.mark.integration
+def test_query_writer_with_non_supported_parameters(client_config: Neo4jClientConfig):
+    class Location:
+        def __init__(self, area: str):
+            self.area = area
+
+    writer = Neo4jQueryWriter(client_config=client_config, verify_connectivity=True, runtime_parameters=["data"])
+
+    runtime_params = {
+        "location": Location("Nice"),
+    }
+
+    with pytest.raises(Exception):  # noqa: B017
+        writer.run(
+            query=("MERGE (p:`Person` {name: $name}) SET p += { area: $location.area }"),
+            parameters=runtime_params,
+        )
 
 
 @pytest.mark.integration
@@ -207,16 +285,13 @@ def test_query_writer_raises_on_failure(client_config: Neo4jClientConfig):
 
 @pytest.mark.integration
 def test_query_writer_return_on_failure(client_config: Neo4jClientConfig):
-    writer = Neo4jQueryWriter(
-        client_config=client_config,
-        verify_connectivity=False,
-        raise_on_failure=False,
-    )
+    writer = Neo4jQueryWriter(client_config=client_config, raise_on_failure=False)
 
     invalid_query = "CREATE (n:Universe {id=`42`})"  # should be ":" instead of "="
     result = writer.run(invalid_query)
 
-    assert result == {"query_type": "w", "query_status": "error"}
+    assert result["query_status"] == "error"
+    assert "Invalid input" in result["error_message"]
 
 
 @pytest.mark.unit
@@ -226,15 +301,20 @@ def test_neo4j_query_writer_to_dict(neo4j_client_mock, client_config_mock):
     neo4j_client = neo4j_client_mock.return_value  # capturing instance created in Neo4jQueryWriter
     client_config_mock.configure_mock(**{"to_dict.return_value": {"mock": "mock"}})
 
-    retriever = Neo4jQueryWriter(
-        client_config=client_config_mock, runtime_parameters=["year"], verify_connectivity=True, raise_on_failure=False
+    writer = Neo4jQueryWriter(
+        client_config=client_config_mock,
+        query="cypher",
+        runtime_parameters=["year"],
+        verify_connectivity=True,
+        raise_on_failure=False,
     )
 
-    data = retriever.to_dict()
+    data = writer.to_dict()
 
     assert data == {
         "type": "neo4j_haystack.components.neo4j_query_writer.Neo4jQueryWriter",
         "init_parameters": {
+            "query": "cypher",
             "runtime_parameters": ["year"],
             "verify_connectivity": True,
             "raise_on_failure": False,
@@ -255,6 +335,7 @@ def test_neo4j_query_writer_from_dict(neo4j_client_mock, from_dict_mock):
     data = {
         "type": "neo4j_haystack.components.neo4j_query_writer.Neo4jQueryWriter",
         "init_parameters": {
+            "query": "cypher",
             "runtime_parameters": ["year"],
             "verify_connectivity": True,
             "raise_on_failure": False,
@@ -262,11 +343,12 @@ def test_neo4j_query_writer_from_dict(neo4j_client_mock, from_dict_mock):
         },
     }
 
-    retriever = Neo4jQueryWriter.from_dict(data)
+    writer = Neo4jQueryWriter.from_dict(data)
 
-    assert retriever._client_config == expected_client_config
-    assert retriever._runtime_parameters == ["year"]
-    assert retriever._verify_connectivity is True
-    assert retriever._raise_on_failure is False
+    assert writer._query == "cypher"
+    assert writer._client_config == expected_client_config
+    assert writer._runtime_parameters == ["year"]
+    assert writer._verify_connectivity is True
+    assert writer._raise_on_failure is False
 
     neo4j_client.verify_connectivity.assert_called_once()
